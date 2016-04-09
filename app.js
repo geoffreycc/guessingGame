77b3610d7a85e2f09cc834a73fcd4e8a0ca0e6ad
geoffreycc <gcox3d@gmail.com>
2016-04-09 13:52:39 -0700

    added .toLowerCase() to the yes or no questions.

--- app.js
+++ app.js
@@ -4,9 +4,9 @@
 
       var answerOne = prompt(userName + ", am I from Washington?");//creates the variable which will prompt the question and store the user input.
       console.log("answerOne is " + answerOne);//displays in the developer console what the user input was.
-      if (answerOne === "yes"){//compares the user input to the string yes.
+      if (answerOne.toLowerCase() === "yes"){//compares the user input in lowercase to the string yes.
         alert("Great job " + userName + "!");//If the above statement is true this string will be displayed.
-      }else if (answerOne === "no"){//compares the user input to the string no.
+      }else if (answerOne.toLowerCase() === "no"){//compares the user input, converted to lower case characters, to the string no.
         alert("Sorry " + userName + " that is incorrect.");//If the above statement is true this string will be displayed.
       }else{//If the user input not 'yes' or 'no' this will run.
         alert("I'm sorry. The only accepted answers are 'yes' and 'no'.");//will be displayed when the above is true.
@@ -14,9 +14,9 @@
 
       var answerTwo = prompt(userName + ", did I work at Sega?");//creates the variable answerTwo which prompts the question and stores the answer in yesNoAnswerTwo.
       console.log("answerTwo is " + answerTwo);//displays the value of answerTwo in the console.
-      if (answerTwo === "no"){//compares the user input to no
+      if (answerTwo.toLowerCase() === "no"){//compares the user input to no
         alert("That is correct " + userName + ".");//if the above statement is true this string will display.
-      }else if (answerTwo === "yes"){//compares the user input to yes.
+      }else if (answerTwo.toLowerCase() === "yes"){//compares the user input to yes.
         alert("I did not work at sega.");//if the above statement is true this string will display.
       }else{//if the user input is not yes or no this will run.
         alert("Only 'yes' and 'no' are accepted for this question.");//displays when the above is ture.
@@ -24,9 +24,9 @@
 
       var answerThree = prompt("Is painting one of my hobbies?");//creates the variable answerThree, prompts the question and stores the answer in the variable.
       console.log("answerThree is " + answerThree);//displays in the value of answerThree in the console.
-      if (answerThree === "yes"){//compares the user input to yes.
+      if (answerThree.toLowerCase() === "yes"){//compares the user input to yes.
         alert("That's right " + userName + " it sure is.");//if the above is true this string will display.
-      }else if (answerThree === "no"){//compares the user input to no.
+      }else if (answerThree.toLowerCase() === "no"){//compares the user input to no.
         alert("Actually it is one of my hobbies.");//if the above is true this string will display.
       }else{//if the answer is not yes and is also not no this will run.
         alert("Sorry only 'yes' and 'no' are recognized.");//this string displays if the above is true.
